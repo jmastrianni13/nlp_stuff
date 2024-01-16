0a2678998c1e6ad59282884eb5c3c349a10b5d49
Joe Mastrianni <jmastrianni13@gmail.com>
2024-01-16 14:52:22 -0500

    added fn to combine text

--- gzip_classifier/src/read_data.rs
+++ gzip_classifier/src/read_data.rs
@@ -7,6 +7,11 @@ pub fn main() {
     let contents = read_into_vec();
     let processed_contents = process_ag_data(contents);
     preview(&processed_contents);
+    let combined_contents = combine_text(processed_contents);
+    for c in combined_contents.iter().take(5) {
+        println!("> {:?}", c);
+    }
+
 }
 
 fn read_into_vec() -> Vec<String> {
@@ -36,9 +41,27 @@ fn process_ag_data(raw_content: Vec<String>) -> Vec<(String, String, String)> {
     return processed;
 }
 
-fn preview<T: Clone + std::hash::Hash + std::cmp::Eq + std::fmt::Debug, U, V>(
+fn combine_text(contents: Vec<(String, String, String)>) -> Vec<(String, String)> {
+    let mut combined = Vec::new();
+
+    for line in contents.iter() {
+        let (class, title, intro) = line;
+        let mut text: String = title.to_string();
+        text.push_str(" ");
+        text.push_str(intro);
+        combined.push((class.to_string(), text.to_string()));
+    }
+
+    return combined;
+
+}
+
+fn preview<T: Clone + std::hash::Hash + std::cmp::Eq + std::fmt::Debug, U: std::fmt::Debug, V: std::fmt::Debug>(
     contents: &Vec<(T, U, V)>,
 ) {
+    for c in contents.iter().take(5) {
+        println!("{:?}", c);
+    }
     let first_values: Vec<&T> = contents.iter().map(|(first, _, _)| first).collect();
     let mut counts = HashMap::new();
     for &value in &first_values {
