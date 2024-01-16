1a71ea82119e02d2542394ee779654a633be1d81
Joe Mastrianni <jmastrianni13@gmail.com>
2024-01-16 12:29:15 -0500

    updated preview to display class distribution

--- gzip_classifier/src/read_data.rs
+++ gzip_classifier/src/read_data.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -11,42 +12,37 @@ pub fn main() {
 fn read_into_vec() -> Vec<String> {
     let file_path = "hf_ag_train.txt";
     let f = fs::File::open(&file_path).expect(&format!("could not read {}", file_path).to_string());
-    // let f = fs::File::open(&file_path).expect("could not read file");
 
     let buffer = BufReader::new(f);
-    let raw_content = buffer.lines()
+    let raw_content = buffer
+        .lines()
         .map(|l| l.expect("could not parse line"))
         .collect::<Vec<_>>();
 
     return raw_content;
-
 }
 
 fn process_ag_data(raw_content: Vec<String>) -> Vec<(String, String, String)> {
     let mut processed = Vec::new();
-    
+
     for line in raw_content.iter() {
         let mut parts = line.splitn(3, ',');
         let class = parts.next().unwrap().trim_matches('"');
         let title = parts.next().unwrap().trim_matches('"');
         let intro = parts.next().unwrap().trim_matches('"');
-        processed.push(
-            (
-            class.to_string(),
-            title.to_string(),
-            intro.to_string(),
-            )
-        );
+        processed.push((class.to_string(), title.to_string(), intro.to_string()));
     }
 
     return processed;
-
 }
 
-fn preview<T: std::fmt::Debug>(contents: &Vec<T>) {
-    for (l, line) in contents.iter().take(20).enumerate() {
-        println!("{l}: {:?}", line);
+fn preview<T: Clone + std::hash::Hash + std::cmp::Eq + std::fmt::Debug, U, V>(
+    contents: &Vec<(T, U, V)>,
+) {
+    let first_values: Vec<&T> = contents.iter().map(|(first, _, _)| first).collect();
+    let mut counts = HashMap::new();
+    for &value in &first_values {
+        *counts.entry(value).or_insert(0) += 1;
     }
+    println!("class distribution: {:?}", counts);
 }
-
-
